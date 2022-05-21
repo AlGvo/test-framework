28ffb833088635500ad41c0c9b67c55f1ec642a0
Alexander Gvozdev <dp161185gav@gmail.com>
2022-05-21 18:09:14 +0300

    Update Page

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -4,7 +4,7 @@ namespace Aigletter\App\Controllers;
 
 class Page
 {
-    public static function view() {
-        echo 'I am a Page view';
+    public static function view($id, $name) {
+        echo 'I am a Page view ID = '. $id . 'Name = ' . $name;
     }
 }

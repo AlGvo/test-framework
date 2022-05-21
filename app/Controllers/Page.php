2036f171868eb3be375590a1d9d98047af1c5c77
Alexander Gvozdev <dp161185gav@gmail.com>
2022-05-21 18:43:21 +0300

    Connect Router Exceptions

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -4,7 +4,9 @@ namespace Aigletter\App\Controllers;
 
 class Page
 {
-    public static function view($id, $name) {
+    public static function view($id = null, $name = null) {
+        // Для проверки 500 ошибки в роутере
+        //       foo();
         echo 'I am a Page view ID = '. $id . 'Name = ' . $name;
     }
 }
--- public/index.php
+++ public/index.php
@@ -7,5 +7,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $config = include __DIR__ . '/../config/main.php';
 
 $app = Aigletter\Framework\Application::getApp($config);
-$app->run();
 
+
+
+try {
+    $app->run();
+} catch (Algvo\Router\Exceptions\BadRequestException $exception) {
+    echo $exception->getMessage();
+} catch (\Algvo\Router\Exceptions\NotFoundException $exception) {
+    echo $exception->getMessage();
+} catch (\Throwable $exception) {
+    echo $exception->getMessage();
+}
